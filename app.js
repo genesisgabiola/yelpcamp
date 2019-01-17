c10d34b40fdb396f82ddc35bcad7a0a28a844240
Genesis Gabiola <genesisbritanicogabiola@gmail.com>
2019-01-17 13:31:25 +0800

    Update listen port

--- app.js
+++ app.js
@@ -48,7 +48,8 @@ app.use('/', indexRoutes);
 app.use('/campgrounds', campgroundRoutes);
 app.use('/campgrounds/:id/comments', commentRoutes);
 
-app.listen(3000 || process.env.PORT, process.env.IP, function() {
+app.listen(process.env.PORT, process.env.IP, function() {
+// app.listen(3000, function() {
   console.log('The YelpCamp Server has Started!');
 });
 
